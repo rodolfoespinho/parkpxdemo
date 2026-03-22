3745e96d18a7d5a3aeb479018d2d604c33ece9ef
rodolfoespinho <132661930+rodolfoespinho@users.noreply.github.com>
2026-03-22 23:27:15 +0000

    Remove SpeedInsights from main component
    Removed SpeedInsights component from rendering.

--- main.jsx
+++ main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ParkPX from './App.jsx'
-import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ParkPX />
-    <SpeedInsights />
   </React.StrictMode>
 )
