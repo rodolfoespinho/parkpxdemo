a566050ff0908058ed0a92bef059689c9d269ee4
Viamar - Ilha da Berlenga <viamar@mac.home>
2026-03-22 23:59:56 +0000

    fix: remove node_modules, add Analytics + SpeedInsights

--- main.jsx
+++ main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ParkPX from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ParkPX />
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 )
